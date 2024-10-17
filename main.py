43b9d58436cf5f1439b3fac3b125320b14db7e25
pinhead <mrmaxc2023@gmail.com>
2024-10-17 16:51:05 -0500

    Added run script, cleaned main.py, added error handling to weather.py

--- main.py
+++ main.py
@@ -5,36 +5,51 @@ att2 = ('name', 'rating', 'addr')
 att3 = ('name', 'rating', 'addr')
 tempAttrac = (att1, att2, att3)
 
+
 def getWeather(loc):
     print('===========================================')
     print(f'Weather for {loc.location["name"]}, {loc.location["region"]}, {loc.location["country"]}:')
-    print(f'Current condtions: {loc.condition}, {loc.current_temperature} °F')
-    print(f'Max temp: {loc.max_temperature}°F | Min temp: {loc.max_temperature}°F')
-    print(f'Chance of parcipitation: {loc.precipitation_chance}')
+    print(f'Current Conditions: {loc.condition} @ {loc.current_temperature}°F with wind speed of {loc.wind_speed} mph')
+    print(f'Max: {loc.max_temperature}°F | Min: {loc.min_temperature}°F')
+    print(f'Chance of Precipitation ({loc.precipitation_type}): {loc.precipitation_chance}%')
+
+
 def getAttractions(loc):
     print('===========================================')
-    print('Things to do around ' + loc + ':\n')   
+    print('Things to do around ' + loc + ':\n')
     for att in tempAttrac:
         print('Name: ' + att[0])
         print('Rating: ' + att[1])
         print('Address: ' + att[2] + '\n')
+
 
 def getFood(loc):
     print('===========================================')
-    print('Places to eat in ' + loc + ':\n')   
+    print('Places to eat in ' + loc + ':\n')
     for att in tempAttrac:
         print('Name: ' + att[0])
         print('Rating: ' + att[1])
         print('Address: ' + att[2] + '\n')
-        
+
 
 print('Welcome to Trip Planner Deluxe™')
 while True:
-    location = input('Enter your destination, or press q to quit: ')
+    location = input('\nEnter your destination, or press q to quit: ')
     if location == 'q':
         break
     print('Your travel information for ' + location + ':')
-    weatherReport = get_weather(location)
+
+    # simple error handler, the get_weather function handles other error stuff inside its file
+    try:
+        weatherReport = get_weather(location)
+        assert weatherReport is not False
+    except AssertionError:
+        continue
+
+    # reuse the exact location that the weather api returns, making it easier to find locations in other api calls
+    precise_location = f"{weatherReport.location['name']}, {weatherReport.location['region']}"
+
+    # prints each location
     getWeather(weatherReport)
-    #getAttractions(location)
-    #getFood(location)
+    # getAttractions(precise_location)
+    # getFood(precise_location)
--- weather.py
+++ weather.py
@@ -7,14 +7,15 @@ from dotenv import load_dotenv
 
 class Weather:
     def __init__(self,
-                 location: tuple,
+                 location: dict,
                  current_temperature: float,
                  max_temperature: float,
                  min_temperature: float,
                  condition: str,
                  daily_condition: str,
                  wind_speed: float,
-                 precipitation_chance: int
+                 precipitation_chance: int,
+                 precipitation_type: str
                  ):
         """
         This represents the combined current and forecast as one class to make it easier when using the retrieved data.
@@ -27,6 +28,7 @@ class Weather:
         :param daily_condition: The overall condition for the full day.
         :param wind_speed: The current wind speed as a float.
         :param precipitation_chance: The current chance of precipitation as a 0-100 integer.
+        :param precipitation_type: The precipitation type as a string.
         """
         self.location = location
         self.current_temperature = current_temperature
@@ -36,6 +38,7 @@ class Weather:
         self.daily_condition = daily_condition
         self.wind_speed = wind_speed
         self.precipitation_chance = precipitation_chance
+        self.precipitation_type = precipitation_type
 
 
 async def current_forecast_weather(key: str, location: str) -> Weather:
@@ -50,21 +53,29 @@ async def current_forecast_weather(key: str, location: str) -> Weather:
                                params={"key": key,
                                        "q": location
                                        }) as response:
+            if response.status != 200:
+                error = await response.json()
+                if error["error"]["code"] == 1006:
+                    raise ValueError("The location entered wasn't found. Please enter a valid location.")
+                raise Exception(f"Status: {response.status}\nHeaders: {response.headers}\nResponse: {await response.json()}")
+
             report = await response.json()
             current = report["current"]
             forecast = report["forecast"]["forecastday"][0]['day']
             if forecast["daily_chance_of_rain"] > forecast["daily_chance_of_snow"]:
                 precipitation = forecast["daily_chance_of_rain"]
+                precip_type = "rain"
             else:
                 precipitation = forecast["daily_chance_of_snow"]
+                precip_type = "snow"
 
             constructed_class = Weather(report["location"], current["temp_f"], forecast["maxtemp_f"],
                                         forecast["mintemp_f"], current["condition"]["text"],
-                                        forecast["condition"]["text"], current["wind_mph"], precipitation)
+                                        forecast["condition"]["text"], current["wind_mph"], precipitation, precip_type)
             return constructed_class
 
 
-def get_weather(location: str) -> Weather:
+def get_weather(location: str) -> Weather | bool:
     """
     Helper function that loads the key and submits the request to the Weather API. Call this to get the weather.
 
@@ -72,10 +83,16 @@ def get_weather(location: str) -> Weather:
     :return: A class containing the current weather and forecast.
     """
     load_dotenv()
-    response = asyncio.run(current_forecast_weather(getenv("WEATHERAPI_KEY"), location))
-    print(response)
-    return response
+    try:
+        response = asyncio.run(current_forecast_weather(getenv("WEATHERAPI_KEY"), location))
+        return response
+    except ValueError as exception:
+        print(exception)
+        return False
+    except Exception as exception:
+        print(f"Could not get current weather!\n{exception}")
+        return False
 
 
 if __name__ == '__main__':
-    get_weather("Menomonie")
+    get_weather("g")  # purely used for testing
